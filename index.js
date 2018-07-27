45875c8cc79fe93cb5592f4c8f8dd71a4f34fa28
Antony Carrizo <antonycarrizo96@gmail.com>
2018-07-27 01:46:01 -0400

    [DEL] Eliminada controlador no necesario.

--- index.js
+++ index.js
@@ -6,38 +6,9 @@ const conf = require('./config');
 const token = conf.telegram_token;
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/echo/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = match[1];
-
-    bot.sendMessage(chatId, "hola");
-});
-
 app.get('/', (req, res) => {
     res.send('BOT RUNING')
 });
-
-bot.onText(/\🗒/, (msg, match) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(
-        chatId,
-        `Muy bien Ingrese el nombre de tu nota`,
-        {
-            "reply_markup": {
-                "keyboard": [['Crear Tablero','🔙']]
-            }
-        })
-
-});
-bot.onText(/\U+2705️/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'aceptado');
-});
-
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(
@@ -48,6 +19,19 @@ bot.onText(/\/start/, (msg) => {
                 "keyboard": [['🗒','✖️']]
             }
         })
+});
+
+bot.onText(/\🗒/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+        chatId,
+        `Muy bien Ingrese el nombre de tu nota`,
+        {
+            "reply_markup": {
+                "keyboard": [['🔙']]
+            }
+        })
+
 });
 
 app.listen(conf.port, () => {
