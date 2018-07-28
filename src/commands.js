c251d5dd392432975d8b530a88aa3648fbbe6271
Antony Carrizo <antonycarrizo96@gmail.com>
2018-07-28 11:24:56 -0400

    [MOD] Agregado el comando nueva nota.

---
+++ src/commands.js
@@ -0,0 +1,22 @@
+const COMMAND_NOTES = "🗒 Notas";
+const COMMAD_NEW_NOTE = "Nueva nota";
+const COMMAND_EDIT_NOTE = "✏️ Editar Nota";
+const COMMAND_DEL_NOTE = "🗑 Borrar Nota";
+const COMMAND_RETURN = "🔙 Atras"
+
+const ALL_COMMANDS = [
+    COMMAND_NOTES,
+    COMMAD_NEW_NOTE,
+    COMMAND_EDIT_NOTE,
+    COMMAND_DEL_NOTE,
+    COMMAND_RETURN
+];
+
+export {
+    ALL_COMMANDS,
+    COMMAND_NOTES,
+    COMMAD_NEW_NOTE,
+    COMMAND_EDIT_NOTE,
+    COMMAND_DEL_NOTE,
+    COMMAND_RETURN
+};
