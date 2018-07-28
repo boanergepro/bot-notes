ef314e456abed5b6366e0cb82e6fc0fb2b29ba44
Antony Carrizo <antonycarrizo96@gmail.com>
2018-07-28 11:26:25 -0400

    [ADD] Archivo para organizar los grupos de comandos.

---
+++ src/keyboards.js
@@ -0,0 +1,17 @@
+import {
+    ALL_COMMANDS,
+    COMMAND_NOTES,
+    COMMAD_NEW_NOTE,
+    COMMAND_EDIT_NOTE,
+    COMMAND_DEL_NOTE,
+    COMMAND_RETURN
+} from "./commands";
+
+const home = [
+    ALL_COMMANDS
+];
+
+
+export default {
+    home
+};
