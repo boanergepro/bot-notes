c43a90638c675200d4531e8288de9e4d558e39f5
Antony Carrizo <antonycarrizo96@gmail.com>
2018-07-30 15:51:46 -0400

    [MOD] Cambiada la estructura de el keyboard home.

--- src/keyboards.js
+++ src/keyboards.js
@@ -8,7 +8,15 @@ import {
 } from "./commands";
 
 const home = [
-    ALL_COMMANDS
+    [
+        COMMAND_NOTES,
+        COMMAD_NEW_NOTE
+    ],
+    [
+        COMMAND_EDIT_NOTE,
+        COMMAND_DEL_NOTE
+    ]
+
 ];
 
 export default {
